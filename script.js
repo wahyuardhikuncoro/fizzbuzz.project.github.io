fa8df4a4ea1067e6d298ef9e520985cd3bddd4f0
wahyuardhikuncoro <62003308+wahyuardhikuncoro@users.noreply.github.com>
2023-01-23 13:02:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var inputData = document.getElementById("data");
+var button = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  var input = inputData.value;
+  var output = "";
+
+  console.log(input);
+
+    if (input == "") {
+        alert("Fill Number In The Box!");
+    } else if (input == 0 ) {
+        output = "Invalid Input Number";
+    } else {
+        if (input % 3 == 0 && input % 5 == 0) {
+            output = "FizzBuzz";
+        } else if (input % 3 == 0) {
+            output = "Fizz";
+        } else if (input % 5 == 0) {
+            output = "Buzz";
+        } else {
+            output = input;
+        }
+    }
+
+  var p = document.createElement("p");
+  p.textContent = output;
+
+  var result = document.getElementById("result");
+  result.appendChild(p);
+});
